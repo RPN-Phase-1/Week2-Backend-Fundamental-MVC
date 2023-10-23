135198f361f7f249a2f07b87a11d997dfe77fa27
yukenz <yuyun.purniawan@gmail.com>
2023-10-23 17:22:04 +0700

    encryptTugas

---
+++ src/CryptoApp.js
@@ -0,0 +1,14 @@
+import CryptoJS from 'crypto-js'
+
+export function encrypt(text, key) {
+
+    return CryptoJS.AES.encrypt(text, key).toString()
+
+}
+
+export function decrypt(encryptedText, key) {
+
+    return CryptoJS.AES.decrypt(encryptedText, key).toString(CryptoJS.enc.Utf8)
+
+}
+
---
+++ src/app.js
@@ -0,0 +1,7 @@
+import Express from 'express'
+
+const app = Express();
+
+app.listen(8080, () => {
+    console.log("Server run on 8080")
+})
